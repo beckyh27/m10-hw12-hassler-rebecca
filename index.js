00de153c29569a4d58f8ce7377d01788ef0f9a18
Becky Hassler <beckyjhassler@gmail.com>
2021-12-03 19:41:22 -0500

    Create Car class and add 3 properties

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // YOUR CODE HERE
+// Your assignment is to create a class named Car in JavaScript. This class will create objects that have three properties:
+// 1. make (String) - The car's manufacturer. E.g. Honda
+// 2. model (String) - The type of model. E.g. Civic
+// 3. year (Integer) - Four digit year the car was made. E.g. 1998
+
+class Car {
+    constructor(make, model, year) {
+        this.make = make
+        this.model = model
+        this.year = year
+    }
+}
