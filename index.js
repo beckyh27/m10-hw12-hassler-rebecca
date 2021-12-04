24e9a8ed5ed6c90f911ef69572fe431987b5c760
Becky Hassler <beckyjhassler@gmail.com>
2021-12-03 20:09:56 -0500

    Delete unneeded comments

--- index.js
+++ index.js
@@ -1,10 +1,4 @@
 // YOUR CODE HERE
-// Your assignment is to create a class named Car in JavaScript. This class will create objects that have three properties:
-// 1. make (String) - The car's manufacturer. E.g. Honda
-// 2. model (String) - The type of model. E.g. Civic
-// 3. year (Integer) - Four digit year the car was made. E.g. 1998
-
-
 
 class Car {
     constructor(make, model, year) {
@@ -12,9 +6,7 @@ class Car {
         this.model = model
         this.year = year
     }
-    //Your class should also have the following methods:
-    // honk - This method should log "BEEP BEEP!" to the console when called.
-    // performMaintenance - This method should use the setTimeout function to log "maintenance complete" to the console after exactly 3 seconds.
+
     honk() {
         console.log("BEEP BEEP!")
     }
@@ -26,15 +18,8 @@ class Car {
  
 }
 
-// After you have created your Car class do the following in the order listed below:
-// 1. Create a variable named mySweetRide and assign it a car created with your class using the following arguments:
-// o make: 'Pontiac'
-// o model: 'Fiero'
-// o make: 1988
 var mySweetRide = new Car('Pontiac', 'Fiero', 1988)
 
-// 2. call mySweetRide's honk method once
 mySweetRide.honk()
 
-// 3. call mySweetRide's performMaintenance method once
 mySweetRide.performMaintenance()
