95038d5784e103c5f7ca6c29c53f1afd0a1ecefb
Becky Hassler <beckyjhassler@gmail.com>
2021-12-03 20:00:11 -0500

    Add honk and maintenance methods

--- index.js
+++ index.js
@@ -4,10 +4,24 @@
 // 2. model (String) - The type of model. E.g. Civic
 // 3. year (Integer) - Four digit year the car was made. E.g. 1998
 
+
+
 class Car {
     constructor(make, model, year) {
         this.make = make
         this.model = model
         this.year = year
     }
+    //Your class should also have the following methods:
+    // honk - This method should log "BEEP BEEP!" to the console when called.
+    // performMaintenance - This method should use the setTimeout function to log "maintenance complete" to the console after exactly 3 seconds.
+    honk() {
+        console.log("BEEP BEEP!")
+    }
+    performMaintenance() {
+        setTimeout(function() {
+            console.log("maintenance complete")
+        }, 3000)
+    }
+ 
 }
