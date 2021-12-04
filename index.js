d1be6a746a1d4fc4737bcf02c7ece3b5bc6df65c
Becky Hassler <beckyjhassler@gmail.com>
2021-12-03 20:07:19 -0500

    Create mySweetRide variable and call methods

--- index.js
+++ index.js
@@ -25,3 +25,16 @@ class Car {
     }
  
 }
+
+// After you have created your Car class do the following in the order listed below:
+// 1. Create a variable named mySweetRide and assign it a car created with your class using the following arguments:
+// o make: 'Pontiac'
+// o model: 'Fiero'
+// o make: 1988
+var mySweetRide = new Car('Pontiac', 'Fiero', 1988)
+
+// 2. call mySweetRide's honk method once
+mySweetRide.honk()
+
+// 3. call mySweetRide's performMaintenance method once
+mySweetRide.performMaintenance()
